2b829959e38d17dee78adc9c21ee944fee10771c
Siana Rizwan <sianarizwan@gmail.com>
2020-10-21 10:33:17 +0600

    registerpage modified

--- src/View/LoginController.java
+++ src/View/LoginController.java
@@ -3,6 +3,8 @@ package View;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -13,6 +15,10 @@ import java.util.ResourceBundle;
 public class LoginController implements Initializable {
     @FXML
     private AnchorPane loginPane;
+    @FXML
+    private TextField usernameTextfield,passwordTextfield;
+    @FXML
+    private Button loginButton,registerButton;
     @FXML
     private void handleRegister() throws IOException {
         AnchorPane pane  = FXMLLoader.load(getClass().getResource("../View/register.fxml"));
--- src/View/RegisterController.java
+++ src/View/RegisterController.java
@@ -3,6 +3,8 @@ package View;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -13,6 +15,10 @@ import java.util.ResourceBundle;
 public class RegisterController implements Initializable {
     @FXML
     private AnchorPane registerPane;
+    @FXML
+    private TextField userIdTextfield, userNameTextfield,emailTextfield,passwordTextfield, retypePasswordTextfield;
+    @FXML
+    private Button backToLoginPageButton,confirmButton;
    @FXML
    private  void handleBackToLoginPage() throws IOException {
        AnchorPane pane  = FXMLLoader.load(getClass().getResource("../View/login.fxml"));
