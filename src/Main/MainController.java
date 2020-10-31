09c52c3adec85ebeec9666450f7c4a591f177719
Siana Rizwan <sianarizwan@gmail.com>
2020-11-01 00:43:34 +0600

    log Out button implemented

--- src/Main/MainController.java
+++ src/Main/MainController.java
@@ -15,12 +15,17 @@ public class MainController implements Initializable {
     @FXML
     private BorderPane employeeDetailsBorderPane;
     @FXML
+    private  AnchorPane mainPane;
+    @FXML
     private void viewDetails() throws IOException{
         AnchorPane pane  = FXMLLoader.load(getClass().getResource("../EmployeeDet/employeeDetails.fxml"));
         employeeDetailsBorderPane.setCenter(pane);
     }
-
-
+    @FXML
+    private void handleLogOut() throws IOException{
+        AnchorPane pane2 = FXMLLoader.load(getClass().getResource("../Login/login.fxml"));
+        mainPane.getChildren().setAll(pane2);
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
