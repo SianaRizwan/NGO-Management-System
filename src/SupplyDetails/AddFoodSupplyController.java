63ca269aa319fca79bb82f527383c98f4479f4d5
SabrinaSusmi <sabrina.su512@gmail.com>
2020-11-27 16:50:13 +0600

    food pane

--- src/SupplyDetails/AddFoodSupplyController.java
+++ src/SupplyDetails/AddFoodSupplyController.java
@@ -1,4 +1,20 @@
 package SupplyDetails;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
 public class AddFoodSupplyController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
 }
+
--- src/SupplyDetails/FoodController.java
+++ src/SupplyDetails/FoodController.java
@@ -1,13 +1,58 @@
 package SupplyDetails;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FoodController implements Initializable {
+
+    @FXML
+    private TableView<?> foodTable;
+
+    @FXML
+    private TableColumn<?, ?> colFoodItemID;
+
+    @FXML
+    private TableColumn<?, ?> colFoodName;
+
+    @FXML
+    private TableColumn<?, ?> colFoodQuantity;
+
+    @FXML
+    private TableColumn<?, ?> colFoodBuyingPrice;
+
+    @FXML
+    private TextField searchFoodTextField;
+
+    @FXML
+    private Label searchFoodLabel;
+
+    @FXML
+    public void handleAddFood(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void handleDeleteFood(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void handleUpdateFood(ActionEvent event) {
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
 }
