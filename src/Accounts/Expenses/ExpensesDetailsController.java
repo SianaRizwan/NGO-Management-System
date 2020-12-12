7c01dd2eae328bc86b5a801f3729b79071d12abd
SabrinaSusmi <sabrina.su512@gmail.com>
2020-12-12 19:28:43 +0600

    Expense ui added

---
+++ src/Accounts/Expenses/ExpensesDetailsController.java
@@ -0,0 +1,14 @@
+package Accounts.Expenses;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ExpensesDetailsController implements Initializable
+{
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
