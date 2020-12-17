b59a7e9ecea8be8c964d30f769b8c51d359b6ae2
Siana Rizwan <sianarizwan@gmail.com>
2020-12-18 00:51:25 +0600

    Ui for Event details have been updated

--- src/EventDetails/CreateEvent/CreateEventController.java
+++ src/EventDetails/CreateEvent/CreateEventController.java
@@ -1,11 +1,15 @@
 package EventDetails.CreateEvent;
 
+import SupplyDetails.Food.ViewFoodDetails.ViewFoodInformationController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
@@ -18,6 +22,24 @@ public class CreateEventController  implements Initializable {
     private void handleBackBtn(ActionEvent event) throws IOException {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("../Events.fxml"));
         createEventPane.getChildren().setAll(pane);
+    }
+    @FXML
+    private void handleViewAssignedEmployees(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../ViewAssignees/viewAssignedEmployees.fxml"));
+        AnchorPane pane = loader.load();
+        Stage stage = new Stage(StageStyle.DECORATED);
+        stage.setTitle("Assigned Employees");
+        stage.setScene(new Scene(pane));
+        stage.show();
+    }
+    @FXML
+    private void handleViewAssignedVolunteers(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../ViewAssignees/viewAssignedVolunteers.fxml"));
+        AnchorPane pane = loader.load();
+        Stage stage = new Stage(StageStyle.DECORATED);
+        stage.setTitle("Assigned Volunteers");
+        stage.setScene(new Scene(pane));
+        stage.show();
     }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
