c934107315bb1c801be28b2a8c7ba65c6675cfb6
SabrinaSusmi <sabrina.su512@gmail.com>
2021-02-02 02:16:31 +0600

    add supply started

--- src/EventDetails/ManageEvent/ManageEventEmployeesController.java
+++ src/EventDetails/ManageEvent/ManageEventEmployeesController.java
@@ -5,10 +5,17 @@ import com.jfoenix.controls.JFXTextField;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class ManageEventEmployeesController {
     @FXML
@@ -40,9 +47,24 @@ public class ManageEventEmployeesController {
 
     }
 
-    @FXML
-    void handleAddVol(MouseEvent event) {
+    private void showList(FXMLLoader loader, String s) throws IOException {
+        AnchorPane pane = loader.load();
+        Stage stage = new Stage(StageStyle.DECORATED);
+        stage.setTitle(s);
+        stage.setScene(new Scene(pane));
+        stage.show();
+    }
 
+    @FXML
+    void handleAddVol() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ViewAssignees/Volunteers/addVolunteers.fxml"));
+        showList(loader, "Assign Volunteers");
+    }
+
+    @FXML
+    void handleViewAssignedVolunteers() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ViewAssignees/Volunteers/viewAssignedVolunteers.fxml"));
+        showList(loader, "Assigned Volunteers");
     }
 
     @FXML
@@ -62,11 +84,6 @@ public class ManageEventEmployeesController {
 
     @FXML
     void handleViewAssignedSupply(MouseEvent event) {
-
-    }
-
-    @FXML
-    void handleViewAssignedVolunteers(MouseEvent event) {
 
     }
 }
