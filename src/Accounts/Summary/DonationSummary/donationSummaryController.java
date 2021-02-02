20504811291393300e03fd1adfce016d3b16626a
Siana Rizwan <sianarizwan@gmail.com>
2021-02-03 01:56:15 +0600

    donation summary with errors

--- src/Accounts/Summary/DonationSummary/donationSummaryController.java
+++ src/Accounts/Summary/DonationSummary/donationSummaryController.java
@@ -1,13 +1,69 @@
 package Accounts.Summary.DonationSummary;
 
+import SupplyDetails.Food.Food;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.Date;
 import java.util.ResourceBundle;
 
-public class donationSummaryController implements Initializable {
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+public class donationSummaryController {
+    @FXML
+    private TextField monthNumber;
+    @FXML
+    private TextField yearNumber;
+    @FXML
+    private TableView<Donation> donationSummaryTable;
 
+    @FXML
+    private TableColumn<Donation, String> colDonID;
+
+    @FXML
+    private TableColumn<Donation, String> colDonorID;
+
+    @FXML
+    private TableColumn<Donation, Date> colDonDate;
+
+    @FXML
+    private TableColumn<Donation, String> colDonorName;
+
+    @FXML
+    private TableColumn<Donation, Integer> colDonAmount;
+
+    donationSummaryModel model = new donationSummaryModel();
+    String donorId;
+    public void displayInformation() {
+
+        setTableViewValue();
+
+        try {
+            donationSummaryTable.setItems(model.getDonationTableRecords(6,2021));
+        } catch (SQLException throwables) {
+            System.out.println("DonationSummaryController: initialize");
+            throwables.printStackTrace();
+        }
+    }
+    private void setTableViewValue() {
+        colDonID.setCellValueFactory(new PropertyValueFactory<>("donationId"));
+        colDonDate.setCellValueFactory(new PropertyValueFactory<>("donDate"));
+        colDonAmount.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        colDonorID.setCellValueFactory(new PropertyValueFactory<>("donorId"));
+        colDonorName.setCellValueFactory(new PropertyValueFactory<>("donorName"));
+    }
+    public void IDOnEnter() {
+        setTableViewValue();
+
+        try {
+            donationSummaryTable.setItems(model.getDonationTableRecords(Integer.parseInt(monthNumber.getText()),Integer.parseInt(yearNumber.getText())));
+        } catch (SQLException throwables) {
+            System.out.println("donationController: initialize");
+            throwables.printStackTrace();
+        }
     }
 }
--- src/Accounts/Summary/DonationSummary/donationSummaryModel.java
+++ src/Accounts/Summary/DonationSummary/donationSummaryModel.java
@@ -1,4 +1,42 @@
 package Accounts.Summary.DonationSummary;
 
+import Person.DonorDetails.Donor;
+import SupplyDetails.Food.Food;
+import Utilities.OracleConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
 public class donationSummaryModel {
+    protected ObservableList<Donation> getDonationTableRecords(int month, int year) throws SQLException {
+        String sql = "select dt.id,dt.don_date,TO_number(To_CHAR(dt.don_date,'MM')) As month=?,TO_number(To_CHAR(dt.don_date,'YY')) As year=?,dt.amount,dt.donor_id,d.name FROM donation_history dt,DOnor d where d.DOnor_id=dt.donor_id";
+                ;
+        ObservableList<Donation> donationList = FXCollections.observableArrayList();
+        try {
+            OracleConnection oc = new OracleConnection();
+            PreparedStatement ps = oc.conn.prepareStatement(sql);
+            ps.setInt(3,month);
+            ps.setInt(4,year);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                String id = rs.getString(1);
+                Date date=rs.getDate(2);
+                int amount=rs.getInt(3);
+                String donorId=rs.getString(4);
+                String donorName=rs.getString(5);
+
+                Donation donation = new Donation(id,date,amount,donorId,donorName);
+                donationList.add(donation);
+            }
+            return donationList;
+        } catch (Exception e) {
+            System.out.println("getTableRecords : DonationHistoryModel");
+            e.printStackTrace();
+            throw e;
+        }
+    }
 }
