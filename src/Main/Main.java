77f4f225c8ac8ae0e7a6a488b383f15a5739014a
Siana Rizwan <sianarizwan@gmail.com>
2021-02-09 03:56:35 +0600

    splash screen modified with ui

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static boolean isSplashLoaded = false;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root;
