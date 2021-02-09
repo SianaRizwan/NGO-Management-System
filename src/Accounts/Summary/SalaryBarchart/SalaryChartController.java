227202e119e9bd1ac86850a618ad90fe78e2ccc2
SabrinaSusmi <sabrina.su512@gmail.com>
2021-02-09 11:35:41 +0600

    Ui and splash modified

--- src/Accounts/Summary/SalaryBarchart/SalaryChartController.java
+++ src/Accounts/Summary/SalaryBarchart/SalaryChartController.java
@@ -7,27 +7,27 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
-class SalaryChartController {
+public class SalaryChartController {
 
-        @FXML
-        private BarChart<String, Double> SalaryChart;
-        @FXML
-        private CategoryAxis x;
-        @FXML
-        private NumberAxis y;
-        @FXML
-        private JFXTextField totalSalaryTextfield;
+    @FXML
+    private BarChart<String, Double> SalaryChart;
+    @FXML
+    private CategoryAxis x;
+    @FXML
+    private NumberAxis y;
+    @FXML
+    private JFXTextField totalSalaryTextfield;
 
-        public void initialize() {
-            XYChart.Series<String, Double> set1 = new XYChart.Series<>();
-            new SalaryChartModel().setTheBarChart(set1);
-            SalaryChart.getData().addAll(set1);
-            totalSalary();
-        }
+    public void initialize() {
+        XYChart.Series<String, Double> set1 = new XYChart.Series<>();
+        new SalaryChartModel().setTheBarChart(set1);
+        SalaryChart.getData().addAll(set1);
+        totalSalary();
+    }
 
-        public void totalSalary(){
-            totalSalaryTextfield.getStyleClass().clear();
-            totalSalaryTextfield.getStyleClass().add("second-text-field");
-            totalSalaryTextfield.setText(Integer.toString(new SalaryChartModel().getTotalSalary()));
-        }
+    public void totalSalary(){
+        totalSalaryTextfield.getStyleClass().clear();
+        totalSalaryTextfield.getStyleClass().add("second-text-field");
+        totalSalaryTextfield.setText(Integer.toString(new SalaryChartModel().getTotalSalary()));
+    }
 }
